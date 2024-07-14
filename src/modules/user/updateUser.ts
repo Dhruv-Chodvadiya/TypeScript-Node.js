8a09f10e52492a3c8b179f871bcd3f5c379e538a
Dhruv <chodvadiyadhruv06@gmail.com>
2024-07-14 22:05:21 +0530

    updateUser

--- src/modules/user/updateUser.ts
+++ src/modules/user/updateUser.ts
@@ -1,11 +1,6 @@
 import { userModel } from "./schema";
 import { User } from "./types";
 
-/* export const updateUser = async (_id: string, query: Object) => {
-  const updateUser = await userModel.findByIdAndUpdate(_id, query);
-  return updateUser as IUser;
-}; */
-
 export const updateUser = async (user: User) => {
   
   const updateUser = await userModel.findByIdAndUpdate(user._id, user.toJSON());
