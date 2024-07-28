b11096dfce71a897255647723f61b0f728ad6ef9
Dhruv <chodvadiyadhruv06@gmail.com>
2024-07-28 21:58:29 +0530

    update Roles

--- src/modules/role/updateRole.ts
+++ src/modules/role/updateRole.ts
@@ -1,27 +1,6 @@
 import { Role } from "./types";
 import { roleModel } from "./schema";
 
-/* export const updateRole = async (_id: string, updateObj: Object) => {
-    const reqPermission: string[] = [
-        ...new Set<string>(role.permissions),
-    ];
-    let updateObj: {
-        roleName?: string;
-        $addToSet?: { permissions: { $each: string[] } };
-      } = {};
-
-      if (role.roleName) {
-        updateObj.roleName = role.roleName;
-      }
-
-      if (role.permissions && role.permissions.length > 0) {
-        updateObj.$addToSet = { permissions: { $each: reqPermission } };
-      }
-  
-      const updateRole = await roleModel.findByIdAndUpdate(_id, updateObj,{ new: true } );
-    
-    return updateRole;
-} */
 
 export const updateRole = async (updateObj: Role) => {
   /* const reqPermission: string[] = [
